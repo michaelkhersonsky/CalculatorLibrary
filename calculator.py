7e9bf19d53fb1d90c7c5350cfc14efd5c87d6564
Michael Khersonsky <michael.khersonsky@gmail.com>
2021-04-19 20:25:53 -0400

    returntype function added and commented

--- calculator.py
+++ calculator.py
@@ -17,3 +17,6 @@ def divide(first_term, second_term):
 def powerof (first_term, second_term):
     return first_term ** second_term
 
+# this function return a type of an argument
+def returntype (type_term)
+    return type(type_term)
